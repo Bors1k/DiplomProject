9b8fb31176a523846d8cefd4d6d52a24afc21a9c
Mikhail <8916misha@gmail.com>
2021-06-02 15:09:12 +0300

    Update tree-view-comp.component.ts

--- Site/Interface/src/app/components/tree-view-comp/tree-view-comp.component.ts
+++ Site/Interface/src/app/components/tree-view-comp/tree-view-comp.component.ts
@@ -37,55 +37,51 @@ export class TreeViewCompComponent {
     this.treeControl.collapseAll()
     this.selectCategoryEvent.emit(1)
   }
-
+  
   async OnInit() {
     let treeViewData: PartsNode[];
     treeViewData = await this.treeViewService.getTreeView()
 
     treeViewData.forEach(element => {
-      // let children: PartsNode[] = [];
-      // let childrensId: number[] = [];
       element.children = []
-      element.childrensId = []
+      // element.childrensId = []
     });
-    let alrHaveId = false;
+    // let alrHaveId = false;
     treeViewData.forEach(firstCicrle => {
       let i = 0;
       treeViewData.forEach(secondCicrle => {
         if (firstCicrle.idTreeViewTable == secondCicrle.parent) {
           firstCicrle.children[i] = secondCicrle
-          firstCicrle.childrensId.forEach(element => {
-            if(element == secondCicrle.idTreeViewTable){
-              alrHaveId = true;
-            }
-          });
-          firstCicrle.childrensId.push(secondCicrle.idTreeViewTable)
-          alrHaveId = false
+          // firstCicrle.childrensId.forEach(element => {
+          //   if(element == secondCicrle.idTreeViewTable){
+          //     alrHaveId = true;
+          //   }
+          // });
+          // firstCicrle.childrensId.push(secondCicrle.idTreeViewTable)
+          // alrHaveId = false
           i++;
         }
       });
     });
-    treeViewData.forEach(firstCicrle => {
-      let i = 0;
-      treeViewData.forEach(secondCicrle => {
-        if (firstCicrle.idTreeViewTable == secondCicrle.parent) {
-          // firstCicrle.childrensId.push(secondCicrle.childrensId)
+    // treeViewData.forEach(firstCicrle => {
+    //   let i = 0;
+    //   treeViewData.forEach(secondCicrle => {
+    //     if (firstCicrle.idTreeViewTable == secondCicrle.parent) {
+    //       // firstCicrle.childrensId.push(secondCicrle.childrensId)
 
-          firstCicrle.childrensId.forEach(element => {
-            if(element == secondCicrle.idTreeViewTable){
-              alrHaveId = true;
-            }
-          });
-          firstCicrle.childrensId.push(secondCicrle.idTreeViewTable)
-          alrHaveId = false
-          i++;
-        }
-      });
-    });
+    //       firstCicrle.childrensId.forEach(element => {
+    //         if(element == secondCicrle.idTreeViewTable){
+    //           alrHaveId = true;
+    //         }
+    //       });
+    //       firstCicrle.childrensId.push(secondCicrle.idTreeViewTable)
+    //       alrHaveId = false
+    //       i++;
+    //     }
+    //   });
+    // });
     this.treeViewData.data = [treeViewData[0]]
     console.log(treeViewData[0])
   }
-
   hasChild = (_:number, node: PartsNode)=>!!node.children && node.children.length > 0;
-
 }
